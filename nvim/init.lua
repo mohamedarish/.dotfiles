f6b8241c17a30bdd33cea95d10ec397e61aaa7f2
mohamed arish <arish1248@gmail.com>
2023-10-10 20:29:14 +0530

    Added more plugins and also re-added background transparency for wezterm

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,20 +2,20 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Lazy.nvim for package management
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+	vim.fn.system {
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable", -- latest stable release
 		lazypath,
-	})
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require("lazy").setup {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -24,12 +24,18 @@ require("lazy").setup({
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 		},
+		opts = {
+			source_selector = {
+				winbar = false,
+				statusline = false,
+			},
+		},
 	},
 	{
-		"joshdick/onedark.vim",
+		"srcery-colors/srcery-vim",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("onedark")
+			vim.cmd.colorscheme "srcery"
 		end,
 	},
 	{
@@ -42,10 +48,11 @@ require("lazy").setup({
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
+					"Neotree",
 				},
-				ignore_focus = {},
+				ignore_focus = {
+					"Neotree"
+				},
 				always_divide_middle = true,
 				globalstatus = false,
 				refresh = {
@@ -72,7 +79,146 @@ require("lazy").setup({
 			},
 		},
 	},
-})
+	{
+		-- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			"folke/neodev.nvim",
+		},
+	},
+	{
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+
+			-- Adds LSP completion capabilities
+			"hrsh7th/cmp-nvim-lsp",
+
+			-- Adds a number of user-friendly snippets
+			"rafamadriz/friendly-snippets",
+		},
+	},
+
+	-- Useful plugin to show you pending keybinds.
+	{ "folke/which-key.nvim", opts = {} },
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help indent_blankline.txt`
+		main = "ibl",
+		opts = {},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+}
+
+local on_attach = function(_, bufnr)
+	-- NOTE: Remember that lua is a real programming language, and as such it is possible
+	-- to define small helper and utility functions so you don't have to repeat yourself
+	-- many times.
+	--
+	-- In this case, we create a function that lets us more easily define mappings specific
+	-- for LSP related items. It sets the mode, buffer and description for us each time.
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
+end
+
+require("neodev").setup()
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+require("lspconfig").lua_ls.setup {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = { "lua" },
+}
+
+local cmp = require "cmp"
+local luasnip = require "luasnip"
+require("luasnip.loaders.from_vscode").lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert {
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete {},
+		["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+}
 
 -- Sync clipboard between OS and Neovim.
 vim.o.clipboard = "unnamedplus"
@@ -88,3 +234,27 @@ vim.o.termguicolors = true
 -- key mappings for filetree
 vim.keymap.set("n", "<leader>o", "<CMD>Neotree toggle<CR>", { desc = "[O]pen filetree" })
 vim.keymap.set("n", "<leader>e", "<CMD>Neotree focus<CR>", { desc = "[E]nable filetree focus" })
+
+local formatters = {
+	lua = "stylua .",
+	rust = "cargo fmt",
+}
+
+vim.keymap.set("n", "<leader>lf", "<CMD>!stylua .<CR>", { desc = "[L]ua [F]ormat" })
+vim.keymap.set("n", "<leader>rf", "<CMD>!cargo fmt<CR>", { desc = "[R]ust [F]ormat" })
+
+local function formatFile(filename)
+	local extension = string.match(filename, "%.([^%.]+)$")
+	local formatterCommand = formatterMap[extension]
+
+	if formatterCommand then
+		os.execute(formatterCommand .. " " .. filename)
+	else
+		print("No formatter found for file extension: " .. extension)
+	end
+end
+
+local function saveFile(filename)
+	print("File saved: " .. filename)
+	formatFile(filename)
+end
--- wezterm/config/colorscheme.lua
+++ wezterm/config/colorscheme.lua
@@ -1,5 +1,5 @@
 local function apply_colorscheme(config)
-	-- config.window_background_opacity = 0.8
+	config.window_background_opacity = 0.8
 	config.color_scheme = "Seti UI (base16)"
 end
 
